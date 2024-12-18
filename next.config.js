9415e4cdca1e96102a4d9b04d3f6cd56aacec3c2
lahirusb97 <lahirushirant@gmail.com>
2024-12-18 06:05:31 +0530

    s

--- next.config.js
+++ next.config.js
@@ -2,18 +2,21 @@
 const path = require('path');
 
 const nextConfig = {
+  // GitHub Pages deployment config
+  env: {
+    NEXT_PUBLIC_URL: `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/${process.env.GITHUB_REPOSITORY?.split('/')[1] || ''}`,
+  },
   output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
-    unoptimized: true
-  },
-  reactStrictMode: false,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'css')],
+    unoptimized: true,
+    domains: ['github.com']
   },
   trailingSlash: true,
-  devIndicators: {
-    buildActivity: false,
+  reactStrictMode: false,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'css')]
   }
 }
 
